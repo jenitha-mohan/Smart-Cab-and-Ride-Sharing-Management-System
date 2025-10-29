f98d7765c364102a99718dca462c1165183a956c
jenitha-mohan <jenithamohan06@gmail.com>
2025-10-29 17:39:51 +0530

    changes in ride controller and service

--- smart_cab/src/main/java/com/example/smart_cab/controller/RideController.java
+++ smart_cab/src/main/java/com/example/smart_cab/controller/RideController.java
@@ -1,5 +1,45 @@
 package com.example.smart_cab.controller;
 
+import com.example.smart_cab.entity.Ride;
+import com.example.smart_cab.service.RideService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/rides")
 public class RideController {
-    
+    @Autowired
+    private RideService rideService;
+
+    @GetMapping
+    public List<Ride> getAllRides(){
+        return rideService.getAllRides();
+    }
+
+    @GetMapping("/{id}")
+    public Ride getRideByIde(@PathVariable Long id){
+        return rideService.getRideById(id).orElseThrow(()->new RuntimeException("Ride not found"));
+
+    }
+
+    @PostMapping
+    public Ride createRide(@RequestBody Ride ride){
+        return rideService.createRide(ride);
+    }
+
+    @PutMapping("/{id}")
+    public Ride updateRide(@PathVariable Long id,@RequestBody Ride rideDetails){
+        return rideService.updateRide(id,rideDetails);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteRide(@PathVariable Long id){
+        rideService.deleteRide(id);
+        return "Ride deleted successfully!";
+
+    }
+
+
+
 }
--- smart_cab/src/main/java/com/example/smart_cab/service/RideService.java
+++ smart_cab/src/main/java/com/example/smart_cab/service/RideService.java
@@ -7,7 +7,53 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class RideService {
+    @Autowired
+    private RideRepository rideRepository;
+
+    public List<Ride> getAllRides(){
+        return rideRepository.findAll();
+    }
     
+    public Optional<Ride> getRideById(Long id){
+        return rideRepository.findById(id);
+    }
+
+    public Ride createRide(Ride ride){
+        return rideRepository.save(ride);
+    }
+
+    
+    public Ride updateRide(Long id, Ride rideDetails) {
+        Ride ride = rideRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Ride not found"));
+
+        if (rideDetails.getPickupLocation() != null)
+            ride.setPickupLocation(rideDetails.getPickupLocation());
+        if (rideDetails.getDropoffLocation() != null)
+            ride.setDropoffLocation(rideDetails.getDropoffLocation());
+        if (rideDetails.getFare() != null)
+            ride.setFare(rideDetails.getFare());
+        if (rideDetails.getStatus() != null)
+            ride.setStatus(rideDetails.getStatus());
+        if (rideDetails.getCompletedAt() != null)
+            ride.setCompletedAt(rideDetails.getCompletedAt());
+        if (rideDetails.getDriver() != null)
+            ride.setDriver(rideDetails.getDriver());
+        if (rideDetails.getCab() != null)
+            ride.setCab(rideDetails.getCab());
+
+        return rideRepository.save(ride);
+    }
+
+    public boolean deleteRide(Long id){
+        if(rideRepository.existsById(id)){
+            rideRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+
     
 }
